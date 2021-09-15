0ff966941651144cd8511a7709c7cffb1b029a72
Joel Eze <joel.eze@rensource.energy>
2021-09-15 01:01:12 +0100

    Created a Singleton instance of the ConcurrentMap

--- src/main/java/com/seerbit/assessment/utils/DBInstance.java
+++ src/main/java/com/seerbit/assessment/utils/DBInstance.java
@@ -1,5 +1,15 @@
 package com.seerbit.assessment.utils;
 
-public class DBInstance {
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
+public class DBInstance {
+    private static ConcurrentMap concurrentMap = null;
+
+    public static ConcurrentMap getDBInstance() {
+        if (concurrentMap == null) {
+            concurrentMap = new ConcurrentHashMap();
+        }
+        return concurrentMap;
+    }
 }
